8d6713a52dd03c16053e4347144438877d7780f1
Blumster <sarkany0@hotmail.com>
2021-12-01 22:01:15 +0100

    Add Day1 solution

---
+++ AdventOfCode/AdventOfCode.Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿static int CalcIncreaseCount(IEnumerable<string> lines, int windowSize)
+{
+    var increaseCount = 0;
+    var values = new List<int>();
+
+    foreach (var line in lines)
+    {
+        var lineValue = int.Parse(line);
+
+        if (values.Count < windowSize)
+        {
+            values.Add(lineValue);
+            continue;
+        }
+
+        var currentSum = values.Sum();
+
+        values.RemoveAt(0);
+        values.Add(lineValue);
+
+        if (values.Sum() > currentSum)
+            ++increaseCount;
+    }
+
+    return increaseCount;
+}
+
+var lines = File.ReadAllLines("input1.txt");
+
+Console.WriteLine($"Increases(1): {CalcIncreaseCount(lines, 1)}");
+Console.WriteLine($"Increases(3): {CalcIncreaseCount(lines, 3)}");
+
