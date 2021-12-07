339d496ada177d06600949ef5fcfd1cee63410bc
Blumster <sarkany0@hotmail.com>
2021-12-07 23:48:10 +0100

    Add Day7

---
+++ AdventOfCode/AdventOfCode.Day7/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var positions = lines[0].Split(',').Select(x => int.Parse(x)).ToList();
+var lowestPosition = positions.Min();
+var highestPosition = positions.Max();
+
+var lowestFuel = int.MaxValue;
+var position = 0;
+
+for (var i = lowestPosition; i <= highestPosition; ++i)
+{
+    var fuel = positions.Sum(x => (int)Math.Abs(x - i));
+    if (fuel < lowestFuel)
+    {
+        lowestFuel = fuel;
+        position = i;
+    }
+}
+
+Console.WriteLine($"Task1: total fuel to reach position {position}: {lowestFuel}");
+
+lowestFuel = int.MaxValue;
+position = 0;
+
+for (var i = lowestPosition; i <= highestPosition; ++i)
+{
+    var fuel = positions.Sum(x =>
+    {
+        var distance = (int)Math.Abs(x - i);
+
+        return distance * (distance + 1) / 2;
+    });
+    if (fuel < lowestFuel)
+    {
+        lowestFuel = fuel;
+        position = i;
+    }
+}
+
+Console.WriteLine($"Task2: total fuel to reach position {position}: {lowestFuel}");
