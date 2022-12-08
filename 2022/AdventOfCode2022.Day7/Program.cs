9b19eceb8163d3f0e046d657486ce691192d4899
Blumster <sarkany0@hotmail.com>
2022-12-09 00:49:14 +0100

    Add 2022 Day 7, 8

---
+++ 2022/AdventOfCode2022.Day7/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Text.RegularExpressions;
+
+var cdCommand = new Regex(@"^\$ cd (.*)$", RegexOptions.Compiled);
+var dirData = new Regex(@"^dir (.*)$", RegexOptions.Compiled);
+var fileData = new Regex(@"^(\d+) (.*)$", RegexOptions.Compiled);
+
+var lines = File.ReadAllLines("input.txt");
+
+var root = new DirectoryEntry("/", null, new(), new());
+var currentDirectory = root;
+
+foreach (var line in lines)
+{
+    if (line == "$ ls")
+        continue;
+
+    var m = cdCommand.Match(line);
+    if (m.Success)
+    {
+        var dest = m.Groups[1].Value;
+        if (dest == "/")
+        {
+            currentDirectory = root;
+        }
+        else if (dest == "..")
+        {
+            currentDirectory = currentDirectory.Parent ?? throw new InvalidOperationException();
+        }
+        else if (currentDirectory.SubDirectories.ContainsKey(dest))
+        {
+            currentDirectory = currentDirectory.SubDirectories[dest];
+        }
+        else
+            throw new InvalidDataException();
+
+        continue;
+    }
+
+    m = dirData.Match(line);
+    if (m.Success)
+    {
+        currentDirectory.SubDirectories.Add(m.Groups[1].Value, new(m.Groups[1].Value, currentDirectory, new(), new()));
+        continue;
+    }
+
+    m = fileData.Match(line);
+    if (m.Success)
+    {
+        currentDirectory.Files.Add(m.Groups[2].Value, new(m.Groups[2].Value, int.Parse(m.Groups[1].Value)));
+        continue;
+    }
+
+    throw new InvalidDataException();
+}
+
+// Part1
+var part1Dirs = root.GetDirectoriesUnderSize(100000);
+
+Console.WriteLine(part1Dirs.Sum(d => d.CalcSize()));
+
+// Part2
+const int totalSize = 70_000_000;
+const int updateSize = 30_000_000;
+
+var currentSize = root.CalcSize();
+
+var part2Dirs = root.GetDirectoriesOverSize(currentSize + updateSize - totalSize);
+
+Console.WriteLine(part2Dirs.Min(d => d.CalcSize()));
+
+record class FileEntry(string Name, int Size);
+
+record class DirectoryEntry(string Name, DirectoryEntry? Parent, Dictionary<string, DirectoryEntry> SubDirectories, Dictionary<string, FileEntry> Files)
+{
+    private int SizeCache { get; set; } = -1;
+
+    public int CalcSize()
+    {
+        if (SizeCache == -1)
+            SizeCache = SubDirectories.Sum(d => d.Value.CalcSize()) + Files.Sum(f => f.Value.Size);
+
+        return SizeCache;
+    }
+
+    public IEnumerable<DirectoryEntry> GetDirectoriesUnderSize(int size)
+    {
+        foreach (var subDir in SubDirectories)
+            foreach (var dir in subDir.Value.GetDirectoriesUnderSize(size))
+                yield return dir;
+
+        if (CalcSize() < size)
+            yield return this;
+    }
+
+    public IEnumerable<DirectoryEntry> GetDirectoriesOverSize(int size)
+    {
+        foreach (var subDir in SubDirectories)
+            foreach (var dir in subDir.Value.GetDirectoriesOverSize(size))
+                yield return dir;
+
+        if (CalcSize() >= size)
+            yield return this;
+    }
+}
---
+++ 2022/AdventOfCode2022.Day8/Program.cs
@@ -0,0 +1,141 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var width = lines[0].Length;
+var height = lines.Length;
+
+var trees = new int[width, height];
+var visibilityCache = new VisibilityCacheEntry[width, height];
+
+for (var x = 0; x < height; ++x)
+{
+    for (var y = 0; y < width; ++y)
+    {
+        trees[x, y] = lines[x][y] - '0';
+
+        visibilityCache[x, y] = new VisibilityCacheEntry
+        {
+            IsVisible = x == 0 || x == height - 1 || y == 0 || y == width - 1
+        };
+    }
+}
+
+for (var x = 1; x < height - 1; ++x)
+    for (var y = 1; y < width - 1; ++y)
+        CalculateVisibility(x, y);
+
+void CalculateVisibility(int x, int y)
+{
+    var height = trees[x, y];
+
+    var isVisible = false;
+    var scenicScore = 1;
+
+    // Top
+    {
+        var allShorter = true;
+        var treeCounter = 0;
+
+        for (var xi = x - 1; xi >= 0; --xi)
+        {
+            ++treeCounter;
+
+            if (trees[xi, y] >= height)
+            {
+                allShorter = false;
+                break;
+            }
+        }
+
+        scenicScore *= treeCounter;
+        isVisible |= allShorter;
+    }
+
+    // Right
+    {
+        var allShorter = true;
+        var treeCounter = 0;
+
+        for (var yi = y + 1; yi < trees.GetLength(1); ++yi)
+        {
+            ++treeCounter;
+
+            if (trees[x, yi] >= height)
+            {
+                allShorter = false;
+                break;
+            }
+        }
+
+        scenicScore *= treeCounter;
+        isVisible |= allShorter;
+    }
+
+    // Bottom
+    {
+        var allShorter = true;
+        var treeCounter = 0;
+
+        for (var xi = x + 1; xi < trees.GetLength(0); ++xi)
+        {
+            ++treeCounter;
+
+            if (trees[xi, y] >= height)
+            {
+                allShorter = false;
+                break;
+            }
+        }
+
+        scenicScore *= treeCounter;
+        isVisible |= allShorter; 
+    }
+
+    // Left
+    {
+        var allShorter = true;
+        var treeCounter = 0;
+
+        for (var yi = y - 1; yi >= 0; --yi)
+        {
+            ++treeCounter;
+
+            if (trees[x, yi] >= height)
+            {
+                allShorter = false;
+                break;
+            }
+        }
+
+        scenicScore *= treeCounter;
+        isVisible |= allShorter;
+    }
+
+    visibilityCache[x, y].IsVisible = isVisible;
+    visibilityCache[x, y].ScenicScore = scenicScore;
+}
+
+// Part1
+var part1Count = 0;
+
+for (var x = 0; x < height; ++x)
+    for (var y = 0; y < width; ++y)
+        if (visibilityCache[x, y].IsVisible)
+            ++part1Count;
+
+Console.WriteLine(part1Count);
+
+// Part2
+var max = 0;
+
+for (var x = 0; x < height; ++x)
+    for (var y = 0; y < width; ++y)
+        if (visibilityCache[x, y].ScenicScore > max)
+            max = visibilityCache[x, y].ScenicScore;
+
+Console.WriteLine(max);
+
+class VisibilityCacheEntry
+{
+    public bool IsVisible { get; set; } = false;
+    public int ScenicScore { get; set; } = 1;
+}
